e7e907dfc6a9ea2e287ca432b26003639176c3e1
Morgan Brown <mbrown7@mail.umw.edu>
2013-10-20 16:53:05 -0400

    Fixing Null Pointer Exception

--- src/edu/umw/cpsc/collegesim/Group.java
+++ src/edu/umw/cpsc/collegesim/Group.java
@@ -9,11 +9,12 @@ import ec.util.*;
 
 public class Group{
 	//all hard coded rands are subject to change
+	int NUM_CONSTANT_ATTRIBUTES = 10; //this is to be changed if morgan changes her constant-- maybe talk to stephen about keeping them synched?
 	private int id;
 	private int size = 0;//based off how many people join-- affects it for now by decreasing the recruitment factor when increased-- gotta think of a way to scale it though to effect the closeness appropriately 
 	private int tightness=0;//based on individual students' willingness to make friends in the group
 	private int frequency;//random 1-10
-	private int recruitmentFactor;//random 1-10
+	private double recruitmentFactor;//random 1-10, slowly decreased as more people join
 	static MersenneTwisterFast rand;
 	
 	
@@ -27,16 +28,22 @@ public class Group{
 	}
 	
 	void recruitStudent(Person s){
-		double r = (2*affinityTo(s) + 2*recruitmentFactor + s.getWillingnessToMakeFriends()+rand.nextInt(10)+1)/6.0; //want to mess with balence here
+		double r = (recruitmentFactor + s.getWillingnessToMakeFriends()+rand.nextInt(10)+1)/3.0;
 		if(r>7){
 			students.add(s);
+			double rf= recruitmentFactor-rand.nextDouble();
+			if(rf>1){
+				recruitmentFactor=rf;
+			}
 		}
 		size = students.size();
-		int t=0;
-		for(int x = 0; x<size; x++){
-			t += students.get(x).getWillingnessToMakeFriends();
+		if(size>0){
+			int t=0;
+			for(int x = 0; x<size; x++){
+				t += students.get(x).getWillingnessToMakeFriends();
+			}
+			tightness = t/size;
 		}
-		tightness = t/size;
 	}
 	
 	private boolean doesGroupContainStudent(Person p){
@@ -67,7 +74,7 @@ public class Group{
 		frequency=f;
 	}
 	
-	public void setRecruitmentFactor(int r){
+	public void setRecruitmentFactor(double r){
 		recruitmentFactor=r;
 	}
 	
@@ -83,12 +90,12 @@ public class Group{
 		return frequency;
 	}
 	
-	public int getRecruitmentFactor(){
+	public double getRecruitmentFactor(){
 		return recruitmentFactor;
 	}
 	
 	public int getCloseness(){
-		return  (tightness+frequency+recruitmentFactor)/3;
+		return (int) (tightness+frequency+recruitmentFactor)/3;
 	}
 
 	public String toString(){
@@ -127,48 +134,8 @@ public class Group{
         //
         // question: what to return from this method if the group is empty?
         // .5?
+
+
     }
-
-    void influnceMembers(){
-    	ArrayList<Double> independentAverage = new ArrayList<Double>();
-    	ArrayList<Double> dependentAverage = new ArrayList<Double>();
-    	double independentTemp;
-    	double dependentTemp;
-    	for (int x = 0; x<independentAverage.size(); x++){		//when all is working, may want to switch x and y-- I use them for opposite tasks lower in the method 
-    		independentTemp=0;
-    		dependentTemp=0;
-    		for (int y = 0; y<students.size(); y++){
-    			independentTemp+=students.get(y).getIndependentAttributes().get(x);
-    			dependentTemp+=students.get(y).getDependentAttributes().get(x);
-    		}
-    		independentAverage.set(x, (independentTemp/students.size()));
-    		dependentAverage.set(x, (dependentTemp/students.size()));
-    	}
-    	//At this point, both independentAverage and dependentAverage are filled
-    	//the following should use two rands-- one to see if the attribute should in fact change, and another to be used to multiply by the distance to calculate how much it would increment by
-    	//note that a group's influence will never directly decrement any attributes-- dependent attributes may only decrement by indirect normalization
-    	//We have to keep our numbers pretty low here-- this will be called at every step
-    	double distanceI;	//distance between current person's current independent attribute and the group's average attribute
-    	double distanceD;	//distance between current person's current dependent attribute and group's average attribute
-    	double increment; //how much each attribute will increment by
-    	for(int x = 0; x<students.size(); x++){
-    		for (int y = 0; y<independentAverage.size(); y++){
-    			distanceI = independentAverage.get(y) - students.get(x).getIndependentAttributes().get(y);
-    			distanceD = dependentAverage.get(y) - students.get(x).getDependentAttributes().get(y);
-
-    			if(rand.nextDouble(true,true)>.97 && distanceI>0){	//rand subject to change 
-    				increment = (rand.nextDouble(true,true)/5)*distanceI; //random number inclusively from 0-1, then divide by 5, then multiply by the distance that attribute is from the group's average
-    				//need method for changing independent attributes that caps attributes at 1
-    			}	
-
-    			if(rand.nextDouble(true,true)>.97 && distanceD>0){	
-    				increment = (rand.nextDouble(true, true)/5)*distanceD;
-    				students.get(x).setAttrValue(y, (students.get(x).getDependentAttributes().get(y))+increment);	//Morgan's method
-    			}
-
-    		}
-    	}
-    }
-
 }
 
